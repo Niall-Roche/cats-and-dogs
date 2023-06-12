8d03f1c08d434046239e89ca496f89dfbd26c77e
Niall Roche <niallroche17@gmail.com>
2023-06-12 17:00:24 +0100

    moved app-theme to src

--- src/utils/test-utils/renderCreate.js
+++ src/utils/test-utils/renderCreate.js
@@ -1,6 +1,6 @@
 import {ThemeProvider} from '@tanstack/react-query-devtools/build/lib/theme'
 import TestRenderer from 'react-test-renderer'
-import theme from '../../../app-theme.json'
+import theme from '../../app-theme.json'
 import {CatsAndDogsProvider} from '../../hooks/useCatsAndDogs'
 import {PaginationProvider} from '../../hooks/usePagination'
 
